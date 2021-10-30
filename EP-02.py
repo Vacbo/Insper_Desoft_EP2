dc4bcf9ff19335c1d4f6986387175b2f37f3c046
Vacbo <pedrobp1@al.insper.edu.br>
2021-10-30 13:10:23 -0300

    Iniciando domino (pronto)

--- EP-02.py
+++ EP-02.py
@@ -13,4 +13,22 @@ def cria_pecas():
     random.shuffle(pecas_domino)
     return pecas_domino
 
-print(cria_pecas())
+#print(cria_pecas())
+
+def inicia_jogo(n_jogadores,pecas_a_distribuir):
+    saida={}
+    for x in range(0,n_jogadores):
+        i=0
+        while i<7:
+            if 'jogadores' not in saida:
+                saida['jogadores']={}
+            if x not in saida['jogadores']:  
+                saida['jogadores'][x]=[pecas_a_distribuir.pop(0)]
+            else:
+                saida['jogadores'][x].append(pecas_a_distribuir.pop(0))
+            i+=1
+    saida['monte']=pecas_a_distribuir
+    saida['mesa']=[]
+    return saida
+
+#print(inicia_jogo(2,cria_pecas()))

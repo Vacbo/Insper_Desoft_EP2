e9e8de01964c6ea47a0205fafb43dd69ad336a99
Gustavo Lindenberg Pacheco <gustavolp1@al.insper.edu.br>
2021-11-02 20:13:22 -0300

    determinando jogadores, sua ordem, mesa e monte

--- EP-02.py
+++ EP-02.py
@@ -93,6 +93,9 @@ def adiciona_na_mesa(peca_a_colocar,mesa):
 
 #Começo do Jogo
 
+#Distribuição de peças
+pecas = cria_pecas()
+
 #Texto Inicial
 print('Bem-vindo(a) ao jogo de Dominó!\nO objetivo do jogo é ser o primeiro jogador a ficar sem nenhuma peça na mão.')
 numero_de_jogadores = input('Digite o número de jogadores (2-4):')
@@ -105,4 +108,14 @@ while not check:
         numero_de_jogadores = input('Digite um número de jogadores válido (2-4):')
     else:
         check = True
-        print('poggers') #texto placeholder
+
+#Função Inicia Jogo
+jogadores_mesa_monte = inicia_jogo(int(numero_de_jogadores), pecas)
+
+ordem = []
+for j in jogadores_mesa_monte['jogadores']:
+    ordem.append(int(j))
+
+random.shuffle(ordem)
+
+print(ordem)

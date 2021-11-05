5375b3f932ee366c80433bbbda541f85163b60e0
Vacbo <pedrobp1@al.insper.edu.br>
2021-11-04 23:31:38 -0300

    fix empate()

--- funcoes.py
+++ funcoes.py
@@ -92,7 +92,8 @@ def empate(jogadores_mesa_monte):
     if jogadores_mesa_monte['monte']==[]:
         for j in jogadores_mesa_monte['jogadores']:
            pp=posicoes_possiveis(jogadores_mesa_monte['mesa'],jogadores_mesa_monte['jogadores'][j])
-           posicoes.append(pp)
+           if not pp==[]:
+            posicoes.append(pp)
     if posicoes==[]:
         return 'empatou'
     return -1

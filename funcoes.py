5248cc707b85bf7075231b69fa4753c3b8cc4e05
Vacbo <pedrobp1@al.insper.edu.br>
2021-11-07 19:43:52 -0300

    add cores

--- funcoes.py
+++ funcoes.py
@@ -93,7 +93,22 @@ def empate(jogadores_mesa_monte):
         for j in jogadores_mesa_monte['jogadores']:
            pp=posicoes_possiveis(jogadores_mesa_monte['mesa'],jogadores_mesa_monte['jogadores'][j])
            if not pp==[]:
-            posicoes.append(pp)
+               posicoes.append(pp)
         if posicoes==[]:
             return 'empatou'
     return -1
+
+def cria_cores(pecas):
+  cores= {0 : '\033[31m'+'0',
+   1 : '\033[32m'+'1',
+   2 : '\033[34m'+'2',
+   3 : '\033[36m'+'3',
+   4 : '\033[35m'+'4',
+   5 : '\033[33m'+'5',
+   6 : '\033[37m'+'6'
+  }
+  for peca in pecas:
+      if peca == pecas[-1]:
+          print('\033[37m'+'['+cores[peca[0]]+'\033[37m'+'|'+cores[peca[1]]+'\033[37m'+']')
+      else:
+          print(('\033[37m'+'['+cores[peca[0]]+'\033[37m'+'|'+cores[peca[1]]+'\033[37m'+']'),end='')
